23f6c39367721fa6de5fe3c1e976e29db59d3c7b
ASARSA <120305720+Asarsa12111@users.noreply.github.com>
2026-05-10 18:38:06 +0300

    Add files via upload

---
+++ CuRSOVIK_BOS/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CuRSOVIK_BOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.lblStatus);
+            this.panelTop.Controls.Add(this.btnClear);
+            this.panelTop.Controls.Add(this.btnSave);
+            this.panelTop.Controls.Add(this.btnAnalyze);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1084, 62);
+            this.panelTop.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(387, 23);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(154, 16);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Статус: готов к анализу";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(264, 14);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(107, 34);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(134, 14);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(124, 34);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Сохранить отчёт";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(12, 14);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(116, 34);
+            this.btnAnalyze.TabIndex = 0;
+            this.btnAnalyze.Text = "Запустить анализ";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // txtReport
+            // 
+            this.txtReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtReport.Font = new System.Drawing.Font("Consolas", 9F);
+            this.txtReport.Location = new System.Drawing.Point(0, 62);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.ReadOnly = true;
+            this.txtReport.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtReport.Size = new System.Drawing.Size(1084, 599);
+            this.txtReport.TabIndex = 1;
+            this.txtReport.WordWrap = false;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            this.saveFileDialog.FileName = "security_report";
+            this.saveFileDialog.Filter = "Текстовый отчёт (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            this.saveFileDialog.Title = "Сохранить отчёт анализатора";
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.txtReport);
+            this.Controls.Add(this.panelTop);
+            this.MinimumSize = new System.Drawing.Size(920, 540);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Анализатор безопасности Windows 8.1";
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
